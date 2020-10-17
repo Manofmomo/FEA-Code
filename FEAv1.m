1d5fc79e085bcb240e3f18a59cf5665f22be7376
TusharNilker <57243220+TusharNilker@users.noreply.github.com>
2020-10-17 20:42:17 +0530

    Current Main Working File
    Default values of variables currently set to corresponding zero values

---
+++ FEAv1.m
@@ -0,0 +1,4 @@
+% Functions List (to be updated sequentially when changes are made)
+% 1) Structure to define nodes and subsequently elements
+node = struct('glob_coord',[0,0],'init_coord',[0,0], 'fin_coord', [0,0], 'stress', [0,0], 'shear_force', [0,0])
+
